5334cfa1549d99cc8c75ccf1e3cc6d319c1067c5
dacarson <44933987+dacarson@users.noreply.github.com>
2026-05-28 18:53:16 -0700

    fix: convert EveWeatherService to ES6 class for HAP-NodeJS compatibility (Eve2 grouping)
    The newer HAP-NodeJS makes Service an ES6 class, which cannot be invoked
    via Service.call(this, ...). This caused "Class constructor Service cannot
    be invoked without 'new'" and prevented the child bridge from starting.
    Convert EveWeatherService to extend Service and call super(), matching the
    ES6 class pattern already used in characteristics.js.

--- util/services.js
+++ util/services.js
@@ -1,6 +1,5 @@
 /* jshint asi: true, esversion: 6, laxbreak: true, laxcomma: true, node: true, undef: true, unused: true */
 
-const inherits = require('util').inherits;
 const CustomUUID = {
 	// Eve UUID
 	EveWeather: 'E863F001-079E-48FF-8F27-9C2605A29F52'
@@ -10,12 +9,14 @@ let CustomService = {};
 
 module.exports = function (Service, Characteristic)
 {
-	CustomService.EveWeatherService = function (displayName, subtype)
-	{
-		Service.call(this, displayName, CustomUUID.EveWeather, subtype);
-		this.addCharacteristic(Characteristic.CurrentTemperature);
-	};
-	inherits(CustomService.EveWeatherService, Service);
+	class EveWeatherService extends Service {
+		constructor(displayName, subtype)
+		{
+			super(displayName, CustomUUID.EveWeather, subtype);
+			this.addCharacteristic(Characteristic.CurrentTemperature);
+		}
+	}
+	CustomService.EveWeatherService = EveWeatherService;
 
 	return CustomService;
 };
